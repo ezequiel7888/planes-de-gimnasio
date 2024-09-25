cf8ac9a1e01c7d2316da0d824f7d4d50f41392cb
ezequiel7888 <ezequieljavier172@gmail.com>
2024-09-25 02:20:03 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace planes_de_gimnasio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listbox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 9);
+            this.label1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLAN Y EJERCICIOS DEL GIMNASIO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(8, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(314, 20);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "ELIJA EL PLAN QUE MAS LE GUSTE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 38);
+            this.button1.TabIndex = 48;
+            this.button1.Text = "AGREGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listbox1
+            // 
+            this.listbox1.FormattingEnabled = true;
+            this.listbox1.Location = new System.Drawing.Point(12, 183);
+            this.listbox1.Name = "listbox1";
+            this.listbox1.ScrollAlwaysVisible = true;
+            this.listbox1.Size = new System.Drawing.Size(144, 134);
+            this.listbox1.TabIndex = 49;
+            this.listbox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listbox1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 50;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 38);
+            this.button2.TabIndex = 51;
+            this.button2.Text = "MOSTRAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 12);
+            this.label2.TabIndex = 52;
+            this.label2.Text = "DEVOLUCION DEL INDICE DEL LISTBOX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "....";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 39);
+            this.button3.TabIndex = 54;
+            this.button3.Text = "LISTAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(378, 183);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(138, 134);
+            this.listBox2.TabIndex = 55;
+            this.listBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(357, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(394, 20);
+            this.label4.TabIndex = 56;
+            this.label4.Text = "ELIJA LOS EJERCICIOS QUE QUIERE HACER";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(361, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 20);
+            this.textBox2.TabIndex = 57;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(462, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 39);
+            this.button4.TabIndex = 58;
+            this.button4.Text = "MENSAJE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(516, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(272, 15);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "DEVOLUCION DEL INDICE DEL LISTBOX ";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(144, 40);
+            this.button5.TabIndex = 60;
+            this.button5.Text = "ACEPTAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(378, 323);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(138, 40);
+            this.button6.TabIndex = 61;
+            this.button6.Text = "ACEPTAR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(522, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 62;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(167, 258);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(141, 13);
+            this.label8.TabIndex = 63;
+            this.label8.Text = "USTED ELIGIO EL PLAN....";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(170, 303);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 82);
+            this.textBox3.TabIndex = 64;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(170, 396);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 42);
+            this.button7.TabIndex = 65;
+            this.button7.Text = "ACEPTAR";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(170, 277);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(138, 20);
+            this.textBox4.TabIndex = 66;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(543, 359);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 37);
+            this.button8.TabIndex = 67;
+            this.button8.Text = "ACEPTAR";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(540, 224);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(168, 13);
+            this.label9.TabIndex = 68;
+            this.label9.Text = "USTED ELIGIO EL EJERCICIO....";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(543, 241);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(139, 20);
+            this.textBox5.TabIndex = 69;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(543, 267);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(99, 86);
+            this.textBox6.TabIndex = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listbox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listbox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace planes_de_gimnasio
+{ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //declaramos las variables
+            string lista;
+            
+            //asignar a la variable valor lo que ingresemos en el cuadro de texto
+   
+            lista = Convert.ToString(textBox1.Text);
+
+            //agregamos lo que tenga la variable valor al listbox
+
+            listbox1.Items.Add(lista);
+
+            //limpiar el cuadro de texto y darle el enfoque
+            textBox1.Text = "";
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //declarar una variable tipo entero
+            string valor;
+            int valor1;
+            valor1 = Convert.ToInt32(textBox1.Text); //lo que ingresamos al cuadro de texto sera el indice
+            if (valor1 > listbox1.Items.Count - 1)
+            {
+                MessageBox.Show("el valor ingresado esta fuera del indice");
+                textBox1.Text = "";
+                textBox1.Focus();
+
+            }
+            else
+            {
+                valor = listbox1.Items[valor1].ToString();
+                //MessageBox.Show("el plan elegido es:" + valor);
+                label3.Text = valor;
+            }
+
+
+        }
+
+        private void listbox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //me muestra el contenido del listbox que yo seleccione
+            textBox1.Text = listbox1.SelectedItem.ToString();
+            //me muestra el indice del contenido seleccionado en el listbox
+            label3.Text = listbox1.SelectedIndex.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //declaramos las variables 
+            string listaejercicios;
+
+            //asignar a la variable valor lo que ingresemos en el cuadro de texto
+
+            listaejercicios = Convert.ToString(textBox2.Text);
+
+            //agregamos lo que tenga la variable valor al listbox
+
+            listBox2.Items.Add(listaejercicios);
+
+            //limpiar el cuadro de texto y darle el enfoque
+            textBox2.Text = "";
+            textBox2.Focus();
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //declarar una variable tipo entero
+            string valor;
+            int valor2;
+            valor2 = Convert.ToInt32(textBox2.Text); //lo que ingresamos al cuadro de texto sera el indice
+            if (valor2 > listBox2.Items.Count - 1)
+            {
+                MessageBox.Show("el valor ingresado esta fuera del indice");
+                textBox2.Text = "";
+                textBox2.Focus();
+            }
+            else
+            {
+                valor = listBox2.Items[valor2].ToString();
+                //MessageBox.Show("el plan elegido es:" + valor);
+                label5.Text = valor;
+
+
+
+
+            }
+        }
+
+        private void listBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            //me muestra el contenido del listbox que yo seleccione
+            textBox2.Text = listBox2.SelectedItem.ToString();
+            //me muestra el indice del contenido seleccionado en el listbox
+            label6.Text = listBox2.SelectedIndex.ToString();
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //me muestra un mensaje del evento de clic del boton aceptar indicando las cantidades de planes agregadas al listbox
+            MessageBox.Show("usted eligio un total de :" + listbox1.Items.Add(textBox1));
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //me muestra un mensaje del evento de clic del boton indicando las cantidades de ejercicios agregados al listbox 
+            MessageBox.Show("usted eligio un total de:" + listBox2.Items.Add(textBox2));
+
+
+            
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //declaramos la variable
+            int valor;
+            valor = Convert.ToInt32(textBox4.Text);
+            //generar nuestro ciclo for i = i +3
+            for (int i = 0; i < valor; i++)
+            {
+                textBox3.Text += i.ToString() + "\r\n";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //declaramos la variable
+            int valor;
+            valor = Convert.ToInt32(textBox5.Text);
+            //generar nuestro ciclo for i = i +3
+            for (int i = 0; i < valor; i++)
+            {
+                textBox6.Text += i.ToString() + "\r\n";
+            }
+        }
+    }
+ }
